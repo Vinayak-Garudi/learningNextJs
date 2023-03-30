70762ec500cf2b5b6a2ab4db150fb282086f2a86
Vinayak Garudi <vinayak.garudi@algonauts.in>
2023-03-30 14:35:58 +0530

    next complete

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router"
+import Image from "next/image"
 import styles from '../styles/index.module.css'
 
 const index = () => {
@@ -8,12 +9,33 @@ const index = () => {
       <div className={styles.indexContainer}>
         {/* for adding multiple classNames like that in bootstrap */}
         <h1 className={`${styles.heading} ${styles.heading_main}`}>Hello World</h1>
-        <p> Directly Targetting Tags</p>
+        {/* inline css */}
+        <p style={{ color: "red", fontWeight: "bold" }}> Directly Targetting Tags</p>
+        {/* css by adding style jsx tag */}
+        <h3>Hello Vinayak</h3>
+        <style jsx>
+          {`
+          h3 {
+            color: darkgreen;
+          }
+          `}
+        </style>
+
         {/* the difference between router.push and router.replace */}
         {/* router.push stores the url history i.e. you can go back */}
         {/* router.replace replaces the url resulting in no option of going back or front */}
-        <button onClick={() => router.replace('/nested/custom')}>Custom Nest</button>
-        <button onClick={() => router.replace('/error')}>Error Page</button>
+
+        {/* adding image in next is little different */}
+
+        {/* to add img by url we need to add domain in next.config.js */}
+        <Image src="https://imgs.search.brave.com/30XfZCHCseYCUxktceLOrL95flggyr2KK-nZb3i8IsE/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC52/dmNQTEFyVl9xem5Y/VnduY2dSRmJnSGFF/SyZwaWQ9QXBp" width="100" height="100" />
+
+        {/* to add img from local folder add img to public folder */}
+        <Image src={'/carimg.jpg'} width="100" height={'100'} />
+        <div>
+          <button onClick={() => router.replace('/nested/custom')}>Custom Nest</button>
+          <button onClick={() => router.replace('/error')}>Error Page</button>
+        </div>
       </div>
     </>
   )
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,9 +1,25 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+
 const profile = () => {
+  const router = useRouter()
+  const [brand, setBrand] = useState("")
+
+  const url = `https://dummyjson.com/products/1`
+  useEffect(() => {
+    axios.get(url).then(res => {
+      console.log(res.data.brand)
+      setBrand(res.data.brand)
+    })
+  }, [url])
   return (
     <>
-    <div className="profile-container">
+      <div className="profile-container">
         <h1>Profile</h1>
-    </div>
+        <h3>{brand}</h3>
+        <button onClick={() => router.push(`/nested/${brand}`)}>apple detail</button>
+      </div>
     </>
   )
 }
